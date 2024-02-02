7500e8cf8396575b8c3bcae8ca6ea0f31bb7911b
riki_dev <erichnyaga1@gmail.com>
2024-02-02 07:22:08 +0300

    new-items & icon

--- fortnite_image/create_image.py
+++ fortnite_image/create_image.py
@@ -1,6 +1,13 @@
 from datetime import datetime
 import fortnite_api
 import json
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
 
 api = fortnite_api.FortniteAPI('9ffab28b-c928-4dfe-a43b-26d3b3b5ce58')
 GREEN = '\033[92m'
@@ -9,6 +16,8 @@ RESET = '\033[0m'
 
 def get_daily_item_shop():
     try:
+        new_items = get_new_items()
+
         current_shop = api.shop.fetch()
         
         date_str = current_shop.raw_data['date']
@@ -18,7 +27,7 @@ def get_daily_item_shop():
 
         data_to_save = current_shop.raw_data['featured']['entries']
 
-        json_data = json.dumps(data_to_save, indent=4)
+        json_data = json.dumps(current_shop.raw_data, indent=4)
 
         # Print to a JSON file
         with open('featured_items.json', 'w') as file:
@@ -37,7 +46,7 @@ def get_daily_item_shop():
         <title>Document</title>
         </head>
         <body>
-        <h1><span>Fortnite Item Shop</span><br /><span>{date}</span><br /><span>Shop Items {len(data_to_save)}</span></h1>
+        <h1><span>Fortnite Item Shop</span><br /><span>{date}</span><br /><span>Shop Items {len(data_to_save)} ({new_items} New Items)</span></h1>
 
         <section>
         """
@@ -59,7 +68,7 @@ def get_daily_item_shop():
             html_content += f"""
             <div>
                 <img src={image} alt="product" />
-                <P>{name}<br /> <span class="price"><img class="v-bucks-logo" src="./v-bucks.jpg" alt="">{price}</span></p>
+                <P>{name}<br /> <span class="price"><img class="v-bucks-logo" src="{current_shop.raw_data['vbuckIcon']}" alt="">{price}</span></p>
             </div>
             """
 
@@ -100,7 +109,7 @@ def get_daily_item_shop():
         width: 99%;
         }
         .v-bucks-logo {
-        width:30px;
+        width:20px;
         }
         .price {
         display: flex;
@@ -124,5 +133,20 @@ def get_daily_item_shop():
         
     except Exception as e:
         print(RED + f"... An error occurred: {e} ...." + RESET)
+
+def get_new_items():
+    options = webdriver.ChromeOptions()
+    options.page_load_strategy = 'none'
+    driver = webdriver.Chrome(options=options)
+    wait = WebDriverWait(driver, 20)
+
+    driver.get('https://fortnite.gg/shop?game=br&different')
+
+    wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a[href="/shop?game=br&different"].filter.active',)))
+    new_items = driver.find_element(By.CSS_SELECTOR, 'a[href="/shop?game=br&different"].filter.active > span').get_attribute('innerHTML')
+
+    driver.execute_script("window.stop();")
+
+    return new_items
 
 get_daily_item_shop()
